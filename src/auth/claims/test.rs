8fdb2b629cb3901a6c63f4c40ae02ef84fbc56d8
Kostas Petrikas <kostas@drivemode.com>
2023-02-17 08:40:14 +0900

    Refactored auth custom claims

---
+++ src/auth/claims/test.rs
@@ -0,0 +1,13 @@
+use super::Claims;
+use serde_json::{Value, to_string};
+
+#[test]
+fn test_claims() {
+    let mut claims = Claims::default();
+    claims.get_mut().insert("foo".into(), Value::String("bar".into()));
+    claims.get_mut().insert("one".into(), Value::Number(1.into()));
+
+    let claims_str = to_string(&claims).unwrap();
+
+    assert_eq!("\"{\\\"foo\\\":\\\"bar\\\",\\\"one\\\":1}\"", &claims_str);
+}
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,7 +3,6 @@ mod test;
 pub mod serialize;
 
 pub use serialize::{
-    Claims, 
     StrEpochMs,
     StrEpochSec,
     I128EpochMs
