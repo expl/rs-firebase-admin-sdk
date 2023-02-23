e9d8b0709a047df47906cefd84b7afd4831b71ab
Kostas Petrikas <kpetrikas@gmail.com>
2023-02-23 16:39:57 +0900

    Added clearning emulator example.

---
+++ examples/clear_emulator/src/main.rs
@@ -0,0 +1,19 @@
+use rs_firebase_admin_sdk::{auth::FirebaseEmulatorAuthService, client::ApiHttpClient, App};
+
+async fn clear_emulator<A, C>(auth_emulator_admin: &A)
+where
+    A: FirebaseEmulatorAuthService<C>,
+    C: ApiHttpClient,
+{
+    println!("Deleting all users!");
+
+    auth_emulator_admin.clear_all_users().await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    let emulator_app = App::emulated("my_project".into());
+    let emulator_admin = emulator_app.auth("http://localhost:9099".parse().unwrap());
+
+    clear_emulator(&emulator_admin).await;
+}
