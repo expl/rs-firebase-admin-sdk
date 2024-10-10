3e020361d1ede9f7c1569a8ae5a4910f6b016177
expl <kpetrikas@gmail.com>
2024-10-11 06:58:19 +0900

    Fixed gcloud CLI authentication method

--- lib/src/credentials/emulator/mod.rs
+++ lib/src/credentials/emulator/mod.rs
@@ -1,11 +1,25 @@
 use super::{Credentials, CredentialsError};
 use error_stack::Report;
 
-#[derive(Debug, Clone, Default)]
-pub struct EmulatorCredentials;
+#[derive(Debug, Clone)]
+pub struct EmulatorCredentials {
+    project_id: String,
+}
+
+impl Default for EmulatorCredentials {
+    fn default() -> Self {
+        Self {
+            project_id: std::env::var("PROJECT_ID").unwrap_or("demo-firebase-project".into()),
+        }
+    }
+}
 
 impl Credentials for EmulatorCredentials {
     async fn get_access_token(&self, _scopes: &[&str]) -> Result<String, Report<CredentialsError>> {
         Ok("owner".into())
+    }
+
+    async fn get_project_id(&self) -> Result<String, Report<CredentialsError>> {
+        Ok(self.project_id.clone())
     }
 }
